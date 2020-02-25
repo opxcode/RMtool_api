14df4d83e96e536704292bd2be9b3b6c006e4d57
Pei Xuan Ong <opx@Peis-MacBook-Pro.local>
2020-02-25 20:25:27 +0800

    update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-    root 'welcome#index'
+  
     def index
         render json: { status: 200, message: "Noticeboard API" }
       end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root 'welcome#index'
   resources :incidentlogs, :incidentquery
 
   get 'scrapper', to: 'webscrapper#getFocusArea'
