8081d1463b6f135ae9b0c91c15132bb74dffd693
Pei Xuan Ong <opx@Peis-MacBook-Pro.local>
2020-02-25 22:40:37 +0800

    CORS origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,14 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:5000'
+    origins 'https://rmtool1.herokuapp.com/Dashboard'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+  allow do
+    origins 'localhost:3001'
 
     resource '*',
       headers: :any,
